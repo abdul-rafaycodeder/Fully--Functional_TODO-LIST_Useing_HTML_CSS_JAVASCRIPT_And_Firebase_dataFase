dad6ab0d4988a9b59440a40a52daa30ef3208a73
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-06 01:58:59 +0500

    coding

--- user-Authentication/firebase.js
+++ user-Authentication/firebase.js
@@ -1,58 +1,3 @@
-const showSignup = document.getElementById("showSignup");
-const showLogin = document.getElementById("showLogin");
-const signupContainer = document.querySelector(".sign-up-container");
-const loginContainer = document.querySelector(".sign-in-container");
-
-
-showSignup.onclick = () => {
-    loginContainer.style.display = "none";
-    signupContainer.style.display = "block";
-}
-
-showLogin.onclick = () => {
-    signupContainer.style.display = "none";
-    loginContainer.style.display = "block";
-}
-
-
-// signup
-document.getElementById("signupForm").addEventListener("submit", function (e) {
-
-    e.preventDefault();
-
-    let name = document.getElementById("signupName").value;
-    let email = document.getElementById("signupEmail").value;
-    let password = document.getElementById("signupPassword").value;
-
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
-
-    alert("Account Created Successfully!");
-
-    signupContainer.style.display = "none";
-    loginContainer.style.display = "block";
-
-});
-
-
-// login
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-
-    e.preventDefault();
-
-    let email = document.getElementById("loginEmail").value;
-    let password = document.getElementById("loginPassword").value;
-
-    let savedEmail = localStorage.getItem("email");
-    let savedPassword = localStorage.getItem("password");
-
-    if (email === savedEmail && password === savedPassword) {
-        alert("Login Successful 🎉");
-    } else {
-        alert("Invalid Email or Password");
-    }
-
-});
 
 //-----------------------------User Authentication----------------------//
 
@@ -79,17 +24,20 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 
-async function signUp() {
+// ---------------------------==> Sign Up <==------------------------------//
+
+
+function signUp() {
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
-            // ...
+            console.log(user)
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // ..
         });
 }
+signUp()
