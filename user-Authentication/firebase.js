1443607377d89ffd81e7e6d42b90e17c2c58629b
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-06 01:49:09 +0500

    coding

--- user-Authentication/firebase.js
+++ user-Authentication/firebase.js
@@ -5,51 +5,91 @@ const loginContainer = document.querySelector(".sign-in-container");
 
 
 showSignup.onclick = () => {
-loginContainer.style.display = "none";
-signupContainer.style.display = "block";
+    loginContainer.style.display = "none";
+    signupContainer.style.display = "block";
 }
 
 showLogin.onclick = () => {
-signupContainer.style.display = "none";
-loginContainer.style.display = "block";
+    signupContainer.style.display = "none";
+    loginContainer.style.display = "block";
 }
 
 
 // signup
-document.getElementById("signupForm").addEventListener("submit",function(e){
+document.getElementById("signupForm").addEventListener("submit", function (e) {
 
-e.preventDefault();
+    e.preventDefault();
 
-let name = document.getElementById("signupName").value;
-let email = document.getElementById("signupEmail").value;
-let password = document.getElementById("signupPassword").value;
+    let name = document.getElementById("signupName").value;
+    let email = document.getElementById("signupEmail").value;
+    let password = document.getElementById("signupPassword").value;
 
-localStorage.setItem("email",email);
-localStorage.setItem("password",password);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
 
-alert("Account Created Successfully!");
+    alert("Account Created Successfully!");
 
-signupContainer.style.display="none";
-loginContainer.style.display="block";
+    signupContainer.style.display = "none";
+    loginContainer.style.display = "block";
 
 });
 
 
 // login
-document.getElementById("loginForm").addEventListener("submit",function(e){
+document.getElementById("loginForm").addEventListener("submit", function (e) {
 
-e.preventDefault();
+    e.preventDefault();
 
-let email = document.getElementById("loginEmail").value;
-let password = document.getElementById("loginPassword").value;
+    let email = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginPassword").value;
 
-let savedEmail = localStorage.getItem("email");
-let savedPassword = localStorage.getItem("password");
+    let savedEmail = localStorage.getItem("email");
+    let savedPassword = localStorage.getItem("password");
 
-if(email === savedEmail && password === savedPassword){
-alert("Login Successful 🎉");
-}else{
-alert("Invalid Email or Password");
-}
+    if (email === savedEmail && password === savedPassword) {
+        alert("Login Successful 🎉");
+    } else {
+        alert("Invalid Email or Password");
+    }
 
 });
+
+//-----------------------------User Authentication----------------------//
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js"
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+
+
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjqSmBKT-AFlfa6QmvXx_h0RcUVT7xKms",
+    authDomain: "todo-list-c87c6.firebaseapp.com",
+    projectId: "todo-list-c87c6",
+    storageBucket: "todo-list-c87c6.firebasestorage.app",
+    messagingSenderId: "847699866982",
+    appId: "1:847699866982:web:772e02a793daa9e55044a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+async function signUp() {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+}
